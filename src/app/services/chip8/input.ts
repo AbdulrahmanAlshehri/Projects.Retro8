63edda11bf6f0f55634361af296070ac29bb4426
mentil <doc0oc@gmail.com>
2020-07-25 12:06:23 +0300

    basic keyboard event

--- src/app/services/chip8/input.ts
+++ src/app/services/chip8/input.ts
@@ -7,8 +7,27 @@ export class Input {
     constructor() {
         this._input = new Array<number>(this.KEYS_COUNT);
         this.initInput();
+        window.addEventListener('keydown', (e) => this.onKeyDown(e));
+        window.addEventListener('keyup', (e) => this.onKeyUp(e));
+
     }
 
+    onKeyDown(e:KeyboardEvent) {
+        console.log(e);
+        console.log(e.key);
+        const keyNumber = parseInt(e.key, 16);
+        if(keyNumber < 16) {
+            this.setKey(keyNumber);
+        }
+    }
+    onKeyUp(e: KeyboardEvent) {
+        console.log(e);
+        console.log(e.key);
+        const keyNumber = parseInt(e.key, 16);
+        if(keyNumber < 16) {
+            this.unsetKey(keyNumber);
+        }
+    }
     initInput(): void {
         for(let i =0; i < this.KEYS_COUNT; i++) {
             this._input[i] = 0;
